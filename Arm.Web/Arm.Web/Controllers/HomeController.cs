7b5b1235f691e7657e8f7d887899593d82883180
hemant <hemantkhanduja@gmail.com>
2017-04-01 22:26:21 +0100

    Controller ,view and model added Formatting done using stylecop and resharper used data annotations for validation custom validation error message is added to modal state

--- Arm.Web/Arm.Web/Controllers/HomeController.cs
+++ Arm.Web/Arm.Web/Controllers/HomeController.cs
@@ -1,17 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="arm">
+//   arm
+// </copyright>
+// <summary>
+//   The home controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Arm.Web.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Arm.Web.Models;
+
+    /// <summary>
+    /// The home controller.
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
         public ActionResult Index()
         {
-            return View();
+            var priceEnquiryViewModel = new PriceEnquiryViewModel();
+
+            // todo:remove these hardcoded products list to take from DB
+            priceEnquiryViewModel.Products.Add(new ProductModel { Name = "Product1", ProductId = 1 });
+            priceEnquiryViewModel.Products.Add(new ProductModel { Name = "Product2", ProductId = 2 });
+            this.ViewBag.ShowMessage = false;
+            return this.View(priceEnquiryViewModel);
         }
 
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <param name="priceEnquiryViewModel">
+        /// The price enquiry view model.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public ActionResult Index(PriceEnquiryViewModel priceEnquiryViewModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                if (priceEnquiryViewModel.SelectedProducts != null && priceEnquiryViewModel.SelectedProducts.Any())
+                {
+                    // todo:call save to db and send message 
+                    this.ViewBag.ShowMessage = true;
+                }
+                else
+                {
+                    this.ModelState.AddModelError("productSelection", "Please select at least one product");
+                }
+            }
+
+            this.ViewBag.ShowMessage = false;
+
+            // todo:remove these hardcoded products list to take from DB
+            priceEnquiryViewModel.Products.Add(new ProductModel { Name = "Product1", ProductId = 1 });
+            priceEnquiryViewModel.Products.Add(new ProductModel { Name = "Product2", ProductId = 2 });
+            return this.View(priceEnquiryViewModel);
+        }
     }
 }
